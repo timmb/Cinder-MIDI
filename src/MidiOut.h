f73d66745be526fd422aca7a6d955192d321db1b
Tim <tim@timmb.com>
2013-01-26 17:55:16 +0000

    Added MidiOut class. Not yet tested thoroughly.

---
+++ src/MidiOut.h
@@ -0,0 +1,14 @@
+//
+//  MidiOut.h
+//  ciEnsemble
+//
+//  Created by Tim Murray-Browne on 26/01/2013.
+//
+//
+
+#ifndef __ciEnsemble__MidiOut__
+#define __ciEnsemble__MidiOut__
+
+#include <iostream>
+
+#endif /* defined(__ciEnsemble__MidiOut__) */
